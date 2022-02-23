0fa05c6b2e56762b1298b1008c8ff9db194f1932
Michael Tang <tngmichael28@gmail.com>
2022-02-22 16:13:34 -0800

    Commented out redirect for testing.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Redirect from '../components/redirect';
+// import Redirect from '../components/redirect';
 import AppContext from '../lib/app-context';
 
 // const styles = {
@@ -16,7 +16,7 @@ import AppContext from '../lib/app-context';
 
 export default class Home extends React.Component {
   render() {
-    if (!this.context.user) return <Redirect to="log-in" />;
+    // if (!this.context.user) return <Redirect to="log-in" />;
 
     return (
       <div>
